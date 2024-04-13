f9537d686c6cb5bcbbcefb7cddf8ac7dd92ae2fa
AyushB08 <102773944+AyushB08@users.noreply.github.com>
2024-04-13 16:42:27 -0700

    removed backend

--- server/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const cors = require("cors")
-
-const app = express()
-app.use(express.json)
-app.use(cors())
-
-mongoose.connect("mongodb+srv://ayushbheemaiah:Ab_356355@cluster0.olcm022.mongodb.net/")
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,49 +2,10 @@ import React, { useState } from 'react';
 import { Link } from 'react-scroll';
 import Logo from "../assets/logo.png";
 import './NavbarStyles.css';
-import axios from "axios";
+
 
 const Navbar = () => {
-  const [showLogin, setShowLogin] = useState(false);
-  const [showSignup, setShowSignup] = useState(false);
-
-  const [loginUsername, setLoginUsername] = useState();
-  const [loginPassword, setLoginPassword] = useState();
-  const [signupPassword, setSignupPassword] = useState();
-  const [signupUsername, setSignupUsername] = useState();
-
-  const handleLoginClick = () => {
-    setShowLogin(true);
-    setShowSignup(false);
-  };
-
-  const closeLoginPopup = () => {
-    setShowLogin(false);
-  };
-
-  const handleSignupClick = () => {
-    setShowSignup(true);
-    setShowLogin(false);
-  };
-
-  const closeSignupPopup = () => {
-    setShowSignup(false);
-  };
-
-  const handleLoginSubmit = (e) => {
-    e.preventDefault()
-    axios.post("", {loginUsername, loginPassword})
-    .then(result => console.log(result))
-    .catch(err => console.log(err))
-  }
-
-  const handleSignupSubmit = (e) => {
-    e.preventDefault()
-    axios.post("", {signupUsername, signupPassword})
-    .then(result => console.log(result))
-    .catch(err => console.log(err))
-  }
-
+  
   return (
     <header>
       <nav className="navbar">
@@ -72,52 +33,10 @@ const Navbar = () => {
               2-Player
             </Link>
           </li>
-          <li className="nav-item">
-            <Link className="nav-link" onClick={handleLoginClick}>Login</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" onClick={handleSignupClick}>Signup</Link>
-          </li>
+          
         </ul>
       </nav>
-      {showLogin && (
-        <div className="login-popup">
-          <div className="login-container">
-            <h2>Login</h2>
-            <form onSubmit = {handleLoginSubmit}>
-              <div className="form-group">
-                <label htmlFor="username">Username:</label>
-                <input type="text" id="username" name="username" onChange={(e) => setLoginUsername(e.target.ariaValueMin)}/>
-              </div>
-              <div className="form-group">
-                <label htmlFor="password">Password:</label>
-                <input type="password" id="password" name="password"  onChange={(e) => setLoginPassword(e.target.ariaValueMin)}/>
-              </div>
-              <button type="submit">Login</button>
-            </form>
-            <button onClick={closeLoginPopup}>Close</button>
-          </div>
-        </div>
-      )}
-      {showSignup && (
-        <div className="login-popup">
-          <div className="login-container">
-            <h2>Signup</h2>
-            <form onSubmit = {handleSignupSubmit}>
-              <div className="form-group">
-                <label htmlFor="new-username">Username:</label>
-                <input type="text" id="new-username" name="new-username"  onChange={(e) => setSignupUsername(e.target.ariaValueMin)}/>
-              </div>
-              <div className="form-group">
-                <label htmlFor="new-password">Password:</label>
-                <input type="password" id="new-password" name="new-password"  onChange={(e) => setSignupPassword(e.target.ariaValueMin)}/>
-              </div>
-              <button type="submit">Signup</button>
-            </form>
-            <button onClick={closeSignupPopup}>Close</button>
-          </div>
-        </div>
-      )}
+      
     </header>
   );
 };
